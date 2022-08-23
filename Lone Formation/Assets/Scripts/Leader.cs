16f2a8bd978eacd6046b9b5d4da4c3e0c9ca91f5
Huntrt <huntrt.business@outlook.com>
2022-08-24 01:46:43 +0700

    Able to add allies in leader

--- Lone Formation/Assets/Scripts/Leader.cs
+++ Lone Formation/Assets/Scripts/Leader.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Leader : MonoBehaviour
@@ -5,6 +6,15 @@ public class Leader : MonoBehaviour
     //Set this class to singleton
 	public static Leader i {get{if(_i==null){_i = GameObject.FindObjectOfType<Leader>();}return _i;}} static Leader _i;
 
+    public List<Allies> allies = new List<Allies>();
 	public Formation formation;
 	public LeaderMovement movement;
+
+	public void AddAllies(Allies add)
+	{
+		allies.Add(add);
+		//todo: Make sure to fix id when remove allies
+		add.id = allies.Count;
+		formation.CreateGoal();
+	}
 }
