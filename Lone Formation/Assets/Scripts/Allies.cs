fbcca1c118d79d34b61310ae7305d41cf3532acc
Huntrt <huntrt.business@outlook.com>
2022-08-24 01:46:09 +0700

    Allies are now follow it formation

--- Lone Formation/Assets/Scripts/Allies.cs
+++ Lone Formation/Assets/Scripts/Allies.cs
@@ -1,18 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Allies : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] Stats stats;
+	[SerializeField] Rigidbody2D rb;
+	Formation formation;
+	public int id;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start()
+	{
+		formation = Leader.i.formation;
+		Leader.i.AddAllies(this);
+	}
+
+    void FixedUpdate()
+	{
+		//Get the goal position at it id in formation 
+		Vector2 goal = formation.goals[id-1].position;
+		//Get direction toward the goal 
+		Vector2 dir = goal - (Vector2)rb.position;
+		//Moving toward that direction of that goal
+		rb.MovePosition(rb.position + (dir * stats.movementSpeed) * Time.fixedDeltaTime);
+	}
 }
