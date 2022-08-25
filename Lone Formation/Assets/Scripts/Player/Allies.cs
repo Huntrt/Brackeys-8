09f3242f62c991628c933430f33446a0ec0b5735
Huntrt <huntrt.business@outlook.com>
2022-08-25 15:10:37 +0700

    Prevent allies from go of out the map

--- Lone Formation/Assets/Scripts/Player/Allies.cs
+++ Lone Formation/Assets/Scripts/Player/Allies.cs
@@ -7,9 +7,11 @@ public class Allies : MonoBehaviour
 	[SerializeField] Rigidbody2D rb;
 	Formation formation;
 	public int id;
+	Map map;
 
 	void Start()
 	{
+		map = Map.i;
 		formation = Leader.i.formation;
 		Leader.i.AddAllies(this);
 	}
@@ -20,6 +22,12 @@ public class Allies : MonoBehaviour
 		Vector2 goal = formation.goals[id-1].position;
 		//Get direction toward the goal 
 		Vector2 dir = goal - (Vector2)rb.position;
+		//Prevent allies from go out of map
+		rb.position = new Vector2
+		(
+			Mathf.Clamp(rb.position.x, -map.scale.x, map.scale.x),
+			Mathf.Clamp(rb.position.y, -map.scale.y, map.scale.y)
+		);
 		//Moving toward that direction of that goal
 		rb.MovePosition(rb.position + (dir * stats.movementSpeed) * Time.fixedDeltaTime);
 	}
