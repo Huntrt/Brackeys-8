e74db9018880eb47117a0feeac07a465cd853f4e
Huntrt <huntrt.business@outlook.com>
2022-08-26 20:20:33 +0700

    Able to buy wrecked allies and add it to formation

--- Lone Formation/Assets/Scripts/Player/Wreck.cs
+++ Lone Formation/Assets/Scripts/Player/Wreck.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Wreck : MonoBehaviour
 {
-    // Start is called before the first frame update
+	[SerializeField] GameObject ship;
+	[SerializeField] int cost;
+	[SerializeField] float speed, hitbox;
+	[SerializeField] Rigidbody2D rb;
+	int alliesLayer;
+
     void Start()
     {
-        
+		//Get the allies layer
+		alliesLayer = LayerMask.GetMask("Allies");
+		//Randomly rotation upon spawn
+        transform.localRotation = Quaternion.Euler(0,0,Random.Range(0f,360f));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Update()
+	{
+		//Create circle hitbox to detect if allies in it
+		RaycastHit2D hit = Physics2D.CircleCast(transform.position, hitbox, Vector2.zero, 0, alliesLayer);
+		//If any allies in this wreck hitbox
+		if(hit) 
+		{
+			//If press buy key while has enough to buy this wreck
+			if(Input.GetKey(KeyManager.i.Buy) && Economic.i.Spended(cost))
+			{
+				Instantiate(ship, transform.position, Quaternion.identity);
+				Destroy(gameObject);
+			}
+		}
+		
+	}
+
+	void FixedUpdate()
+	{
+		//Move the wreck backward of the red arrow with speed has get
+		rb.MovePosition(rb.position + (Vector2.left * speed) * Time.fixedDeltaTime);
+	}
 }
