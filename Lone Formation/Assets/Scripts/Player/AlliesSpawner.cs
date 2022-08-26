29ec0627bfbb08d9ef701f17a747dd54af12c41c
Huntrt <huntrt.business@outlook.com>
2022-08-26 20:20:14 +0700

    Spawning wrecked allies

--- Lone Formation/Assets/Scripts/Player/AlliesSpawner.cs
+++ Lone Formation/Assets/Scripts/Player/AlliesSpawner.cs
@@ -3,10 +3,38 @@ using UnityEngine;
 public class AlliesSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] alliesToUse;
-    [SerializeField] Table wreckTable, assistTable;
+    [SerializeField] Table wreckTable; //consider: could use assist
+	Map map;
+
 	[System.Serializable] class Table
 	{
 		public float delay, perSecond; //consider: might scale with difficulty
-	    public WeightData[] assistTable;
+		[HideInInspector] public float timer;
+	    public WeightData[] weightList;
+	}
+
+	void Start()
+	{
+		map = Map.i;
+	}
+
+	void Update()
+	{
+		CountingSpawn();
+	}
+
+	void CountingSpawn()
+	{
+		wreckTable.timer += Time.deltaTime;
+		if(wreckTable.timer >= wreckTable.delay) {SpawnWreck(); wreckTable.timer -= wreckTable.timer;}
+	}
+
+	void SpawnWreck()
+	{
+		//Get randomly position to spawn in Y axis of map border
+		Vector2 pos = new Vector2(map.scale.x, Random.Range(-map.scale.y, map.scale.y));
+		//Weight tochoose which wreck will be spawn
+		GameObject spawn = General.WeightingResult(wreckTable.weightList);
+		Instantiate(spawn, pos, Quaternion.identity);
 	}
 }
